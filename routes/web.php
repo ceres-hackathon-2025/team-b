ab99305854598a20b33a32e74d26d2410c7289b8
Takagi Yuto <yutotkg.1040@gmail.com>
2025-08-08 01:44:13 +0900

    web.php内のroute整理と検索ページのroutes追加

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-
-//controllers
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\SearchApiController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,11 +16,14 @@ Route::middleware('auth')->group(function () {
 });
 
 // 認証
-
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
 Route::post('/signup', [AuthController::class, 'register']);
 
-
+// ログアウト(ログイン時のみ有効)
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+
+// 検索ページ
+Route::get('/search', fn () => view('search.index'));          // 画面
+Route::get('/api/search', [SearchApiController::class, 'index']); // JSON
