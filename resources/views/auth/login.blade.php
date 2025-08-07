5f9acc25a9c23b16549a1b8270144f4e1ab20cca
yui-szk <yuisuzuki.017@gmail.com>
2025-08-07 21:41:06 +0900

    update: change layout of login and signup forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
     <p style="color: green;">{{ session('success') }}</p>
 @endif
 
-<section class="bg-gray-50 dark:bg-gray-900">
+<section class="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-8 px-4 sm:px-6 lg:px-8">
     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
