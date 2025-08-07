e7e642e29846bab4fbd3d97b4620beeaaf7f1f69
kisana <kisana@amiverse.net>
2025-08-07 13:30:05 +0900

    ADD: 共通レイアウトファイル作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title') しっくりボーカル</title>
+</head>
+<body>
+    <header>
+      しっくりボーカル
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+      ©しっくりボーカル
+    </footer>
+</body>
+</html>
