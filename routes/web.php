3a2dd0f1be75ccfbcc16a899bb12bdc623080ca8
Takagi Yuto <yutotkg.1040@gmail.com>
2025-08-07 19:06:59 +0900

    authへのmiddlewareを削除しました

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ Route::middleware('auth')->group(function () {
 });
 
 // 認証
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
-    Route::post('/signup', [AuthController::class, 'register']);
-});
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
+Route::post('/signup', [AuthController::class, 'register']);
+
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
