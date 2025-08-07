92cd3941568b7a974e4c97e04c36b08bcf065eb8
yui-szk <yuisuzuki.017@gmail.com>
2025-08-07 14:28:08 +0900

    fix(layout): remove unnecessary newline and reorganize structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') しっくりボーカル</title>
 </head>
@@ -11,5 +10,12 @@
       しっくりボーカル
     </header>
 
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+      ©しっくりボーカル
+    </footer>
 </body>
 </html>
