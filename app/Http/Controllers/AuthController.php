efcef9ecc432909705339fe335a370c8f4a4b344
kisana <kisana@amiverse.net>
2025-08-08 13:21:19 +0900

    FIX: ログイン後はTLへ

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/dashboard');
+            return redirect()->intended('/posts');
         }
 
         return back()->withErrors(['email' => 'メールアドレスまたはパスワードが正しくありません。']);
