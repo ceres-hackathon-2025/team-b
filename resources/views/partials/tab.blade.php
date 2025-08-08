587621bb360d4d7a54b63288d6bbad5c4cf72e85
黒須祥文 <yoshifumikurosu1547@gmail.com>
2025-08-08 15:07:33 +0900

    ヘッダー？のアイコンのやつ実装

--- resources/views/partials/tab.blade.php
+++ resources/views/partials/tab.blade.php
@@ -1,8 +1,10 @@
-<div class="sticky top-0 flex justify-center bg-white text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-    <div class="flex items-center justify-center w-full max-w-7xl px-4 py-2 max-w-screen-xl mx-auto">
-        <a href="{{ route('home') }}" class="flex-shrink-0">
-            <img src="{{ asset('/444.png') }}" alt="ログイン" class="h-8 w-auto cursor-pointer">
+<div class="sticky top-0 flex justify-between bg-white text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+
+        <a href="{{ route('home') }}" class="flex-shrink-0 ml-2">
+            <img src="{{ asset('/666.png') }}"  class="h-11 w-auto cursor-pointer mt-1">
         </a>
+
+
         <ul class="flex flex-wrap -mb-px">
             <li class="me-2">
                 <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">おすすめ</a>
@@ -11,5 +13,7 @@
                 <a href="#" class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">ランダム</a>
             </li>
         </ul>
-    </div>
+
+    <div class ="w-12"></div>
+
 </div>
